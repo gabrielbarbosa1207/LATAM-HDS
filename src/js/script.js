1e2ef94a2626c5a90b7b6a0c15b610efde67b150
gabrielbarbosaweb <gabrielbarbosasa2@gmail.com>
2023-09-05 11:47:20 -0300

    all

--- src/js/script.js
+++ src/js/script.js
@@ -4,5 +4,5 @@ document.addEventListener("DOMContentLoaded", function() {
         for(var i = 0; i < btns.length; i++){
             btns[i].style.display = "block"
         }
-    }, 474000); // 465000 milliseconds or 7 minutes 45 seconds
+    }, 472000); // 465000 milliseconds or 7 minutes 45 seconds
 });
