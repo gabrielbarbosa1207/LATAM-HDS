eb8dd13cf785048eb5ee7380fbda2469b285bd03
gabrielbarbosa1207 <108965725+gabrielbarbosa1207@users.noreply.github.com>
2023-09-05 15:52:13 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        var btns = document.querySelectorAll("button");
+        for(var i = 0; i < btns.length; i++){
+            btns[i].style.display = "block"
+        }
+    }, 472000); // 465000 milliseconds or 7 minutes 45 seconds
+});
