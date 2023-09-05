a61853530a752b5c2b6c01ac144c30b9ac965ed6
gabrielbarbosa1207 <108965725+gabrielbarbosa1207@users.noreply.github.com>
2023-09-05 15:51:33 -0300

    Delete src directory

--- src/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        var btns = document.querySelectorAll("button");
-        for(var i = 0; i < btns.length; i++){
-            btns[i].style.display = "block"
-        }
-    }, 472000); // 465000 milliseconds or 7 minutes 45 seconds
-});
