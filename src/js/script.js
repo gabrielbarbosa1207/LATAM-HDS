d0e0c172eaef47c6e99202ba2d91516eec73fee0
gabrielbarbosaweb <gabrielbarbosasa2@gmail.com>
2023-09-04 23:46:29 -0300

    all

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        var btn = document.getElementsByClassName("button");
+        if (btn) {
+            btn.style.display = "block";
+        }
+    }, 465000); // 465000 milliseconds or 7 minutes 45 seconds
+});
